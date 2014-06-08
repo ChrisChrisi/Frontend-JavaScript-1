be83a0e869e884a6e7782ba42e92ffb8d3137338
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-09 00:53:05 +0300

    solved sumOfDivisors

--- week0/problems/solutions/sumOfDivisors.js
+++ week0/problems/solutions/sumOfDivisors.js
@@ -1,1 +1,9 @@
 // solution here
+var sum_of_divisors = function(n) {
+
+// generate array of numbers from 1 to n
+    var arr = Array.apply(null, {length: Math.abs(n+1)}).map(Number.call, Number);
+    arr = arr.slice(1);
+
+    return arr.filter(function(a){return (n % a === 0); }).reduce(function(a,b){ return a + b;});
+};
