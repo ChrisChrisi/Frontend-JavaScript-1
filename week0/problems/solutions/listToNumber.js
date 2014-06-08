50e3aa87dd55de6d4e97b318e4ffb86a72ffaca9
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-09 00:20:12 +0300

    solved listToNUmber

--- week0/problems/solutions/listToNumber.js
+++ week0/problems/solutions/listToNumber.js
@@ -1,1 +1,15 @@
 // solution here
+var list_to_number = function(digits){
+    if(!Array.isArray(digits)){
+       throw new TypeError("the argument should be array");
+    }
+
+    if(!digits.every(function(a){
+        return (typeof a === "number"  && a%1 === 0)
+    })){
+        throw new TypeError("the array's elements should be integer number");
+    }
+    return digits.reduce(function(a, b){
+        return a.toString() + b.toString();
+    });
+};
