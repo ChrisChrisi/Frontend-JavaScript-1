e7582b0cf6a3d4b12e061e67982ce6afa9da5dcd
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-08 23:51:35 +0300

    solved isIncreasing

--- week0/problems/solutions/isIncreasing.js
+++ week0/problems/solutions/isIncreasing.js
@@ -1,1 +1,10 @@
 // solution here
+
+var is_increasing = function (seq) {
+    if(!Array.isArray(seq)){
+        throw new TypeError('Invalid parameter');
+    }
+    return !(seq.some(function (a, i) {
+        return a >= seq[i + 1];
+    }));
+};
