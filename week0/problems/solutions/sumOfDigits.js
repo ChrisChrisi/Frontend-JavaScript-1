ba86d9608f839c3a4017fb926f244169ba46509c
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-09 00:50:23 +0300

    solevd sumOfDigits

--- week0/problems/solutions/sumOfDigits.js
+++ week0/problems/solutions/sumOfDigits.js
@@ -1,1 +1,6 @@
 // solution here
+var sumOfDigits = function(n){
+    return n.toString().split("").map(function(a){return parseInt(a);}).filter(function(a){return !isNaN(a) ;}).reduce(function(a,b){return a + b;});
+};
+
+exports.sumOfDigits = sumOfDigits;
