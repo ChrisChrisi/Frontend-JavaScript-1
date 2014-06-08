eef2f326c42df06f2e6ee4f2bbb50ff2ad1f356b
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-09 00:57:53 +0300

    solved sumOfMinAndMax

--- week0/problems/solutions/sumOfMinAndMax.js
+++ week0/problems/solutions/sumOfMinAndMax.js
@@ -1,1 +1,6 @@
 // solution here
+var sum_of_min_and_max = function(arr) {
+    var sortedArr = arr.sort(function(a,b){return (a - b);});
+
+    return (sortedArr[0] +  sortedArr[sortedArr.length - 1] );
+}
