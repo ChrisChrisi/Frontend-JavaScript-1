dd095473b8cadd1df5fe85eb7a696bc0dd71a567
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-08 23:58:02 +0300

    solved isIntPalindrom

--- week0/problems/solutions/isIntPalindrom.js
+++ week0/problems/solutions/isIntPalindrom.js
@@ -1,1 +1,17 @@
 // solution here
+var isIntPalindrome = function(n) {
+    var  numArray = n.toString().split("");
+    var dcount = numArray.length;
+    var del1 = parseInt(dcount/2);
+    var del2;
+    if(dcount % 2 === 0){
+        del2 = del1;
+    }else{
+        del2 = del1 +1;
+    }
+    var firstHalf = numArray.slice(0,del1 );
+    var secondHalf = numArray.slice(del2).reverse();
+
+
+    return firstHalf.sort().toString() === secondHalf.sort().toString();
+};
