83bd35e813cfbb4c78849e3b9295c0b55402cfe2
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-09 01:02:34 +0300

    solved vowels in a string

--- week0/problems/solutions/vowelsInAString.js
+++ week0/problems/solutions/vowelsInAString.js
@@ -1,1 +1,20 @@
 // solution here
+var allVowels = "aeiouy";
+
+//haystack.indexOf(needle) >= 0
+
+var count_vowels = function(str){
+    if(typeof str !== "string"){
+        throw new TypeError("The input should be string");
+    }
+
+    var letters = str.toLowerCase().split("");
+    var result = 0;
+
+    letters.forEach(function(a){
+        if(allVowels.indexOf(a) >= 0){
+            result += 1;
+        }
+    });
+    return result;
+};
