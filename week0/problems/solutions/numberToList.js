029f5cea400fa307176aae3f367a5c219a12e3f3
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-09 00:24:48 +0300

    solved numberToList

--- week0/problems/solutions/numberToList.js
+++ week0/problems/solutions/numberToList.js
@@ -1,1 +1,7 @@
 // solution here
+var number_to_list = function(n){
+    if(typeof n != "number"){
+        throw new TypeError("the argument should be number");
+    }
+    return n.toString().split("");
+};
