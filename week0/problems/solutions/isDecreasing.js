2090a2b64a07a98f17c7d83cd17088d6d70115e7
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-08 23:46:04 +0300

    solved isDecreasing

--- week0/problems/solutions/isDecreasing.js
+++ week0/problems/solutions/isDecreasing.js
@@ -1,1 +1,9 @@
 // solution here
+var isDecreasing = function(seq) {
+    if(!Array.isArray(seq)){
+        throw new TypeError('Invalid parameter');
+    }
+    return !(seq.some(function(a, i){
+        return (a <= seq[i+1]);
+    }));
+};
