2a128c9320825ac68ee2f3e79584950ab10f8655
ChrisChrisi <Chris1Cros@gmail.com>
2014-06-09 00:45:03 +0300

    solved stringFormat

--- week0/problems/solutions/stringFormat.js
+++ week0/problems/solutions/stringFormat.js
@@ -1,1 +1,21 @@
 // solution here
+var format = function (string, dict) {
+    if(typeof string !== "string" || typeof dict !== "object"){
+        throw new TypeError("The first argument should be string, the second dictionary ");
+    }
+    var words = string.match(/{([^}]+)}/g).map(function (a) {
+        return a.replace(/[{}]/g, "")
+    });
+    words.forEach(function (word) {
+        // if something is in brackets, but there's no suck key in the dictionary it cannot be replaced.
+        if(typeof dict[word] != 'undefined'){
+            string = string.replace('{' + word + '}', dict[word]);
+        }
+    });
+
+    return string;
+};
+var dict = {
+    "lang" : "JavaScript",
+    "thing": "language"
+};
