af4d178568373f4c4ac5d95c13b3a193333ea090
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 16:05:39 +0300

    align

--- week0/problems/solutions/sumOfDivisors-test.js
+++ week0/problems/solutions/sumOfDivisors-test.js
@@ -1,4 +1,4 @@
- /*
+/*
  * Solution by Christina Mileva
  * GitHub - https://github.com/ChrisChrisi
  */
@@ -7,31 +7,31 @@
 
 var sumOfDivisors = require("./sumOfDivisors").sumOfDivisors;
 
-exports.testSumOfDivisorsThrow = function(test){
-    test.throws = function(){
+exports.testSumOfDivisorsThrow = function (test) {
+    test.throws = function () {
         sumOfDivisors("dsffsa");
     };
     test.done();
 };
 
-exports.testSumOfDivisorsThrow1 = function(test){
-    test.throws = function(){
+exports.testSumOfDivisorsThrow1 = function (test) {
+    test.throws = function () {
         sumOfDivisors(5.3);
     };
     test.done();
 };
 
-exports.testSumOfDivisors = function(test){
-    test.equal(15,sumOfDivisors(8));
+exports.testSumOfDivisors = function (test) {
+    test.equal(15, sumOfDivisors(8));
     test.done();
 };
 
-exports.testSumOfDivisors1 = function(test){
-    test.equal(8,sumOfDivisors(7));
+exports.testSumOfDivisors1 = function (test) {
+    test.equal(8, sumOfDivisors(7));
     test.done();
 };
 
-exports.testSumOfDivisors = function(test){
-    test.equal(1,sumOfDivisors(1));
+exports.testSumOfDivisors = function (test) {
+    test.equal(1, sumOfDivisors(1));
     test.done();
 };
