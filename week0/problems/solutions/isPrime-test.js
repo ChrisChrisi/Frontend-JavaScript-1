e767294d45757a0d8cf562bcd59a792c94821f51
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 09:33:37 +0300

    isPrime solution and test

---
+++ week0/problems/solutions/isPrime-test.js
@@ -0,0 +1,44 @@
+"use strict";
+
+var isPrime = require("./isPrime").isPrime;
+
+exports.testIsPrimeThrow = function (test) {
+    test.throws = function () {
+        isPrime("sdgfdgh");
+    }
+    test.done();
+};
+
+exports.testIsPrimeThrow1 = function (test) {
+    test.throws = function () {
+        isPrime([1, 3]);
+    }
+    test.done();
+};
+
+exports.testIsPrimeThrow2 = function (test) {
+    test.throws = function () {
+        isPrime(2.3);
+    }
+    test.done();
+};
+
+exports.testIsPrime1 = function (test) {
+    test.equal(false, isPrime(1));
+    test.done();
+};
+
+exports.testIsPrime2 = function (test) {
+    test.equal(true, isPrime(2));
+    test.done();
+};
+
+exports.testIsPrimeTrue = function (test) {
+    test.equal(true, isPrime(89));
+    test.done();
+};
+
+exports.testIsPrimeFalse = function (test) {
+    test.equal(false, isPrime(88));
+    test.done();
+};
--- week0/problems/solutions/isPrime.js
+++ week0/problems/solutions/isPrime.js
@@ -1,1 +1,25 @@
 // solution here
+"use strict";
+
+var isPrime = function(n) {
+    if(typeof n !== "number" || n%1 !== 0){
+        throw new TypeError("The input should be integer!");
+    }
+if(n === 2){
+	return true;
+}
+if (n === 1){
+	return false;
+}
+var arr = Array.apply(null, {length: (Math.abs(n)+1)}).map(Number.call, Number);
+ arr = arr.slice(2);
+
+if(arr.filter(function(a){return (Math.abs(n) % a === 0); }).reduce(function(a,b){ return a + b;}) === Math.abs(n)){
+	return true;
+}
+else {
+	return false;
+}
+};
+
+exports.isPrime = isPrime;
