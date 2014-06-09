0687f60afdbaa7164d05c9f86cb3bde05f57fa5d
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 14:54:43 +0300

    spaces

--- week0/problems/solutions/higherOrder-test.js
+++ week0/problems/solutions/higherOrder-test.js
@@ -1,14 +1,19 @@
+ /*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */ 
+ 
 "use strict";
 
 var functions = require("./higherOrder");
 
 var map = functions.map;
-var mult = function(a){
+var mult = function(a) {
     return a*a;
 };
 
-var even = function(a){
-    if(a % 2 === 0){
+var even = function(a) {
+    if(a % 2 === 0) {
         return true;
     }
     else{
@@ -16,110 +21,110 @@ var even = function(a){
     }
 };
 
-var add = function(a,b){
+var add = function(a,b) {
     return a+b;
 };
 
 var arr = [1,2,3,4];
-exports.testMapThrows = function(test){
-    test.throws = function(){
+exports.testMapThrows = function(test) {
+    test.throws = function() {
         map(mult, "dfgfdfg");
     };
     test.done();
 };
 
-exports.testMap = function(test){
+exports.testMap = function(test) {
     test.deepEqual([1,4,9, 16], map(mult, arr));
     test.done();
 };
 
-exports.testMap1 = function(test){
+exports.testMap1 = function(test) {
     test.deepEqual([], map(mult, []));
     test.done();
 };
 
 var filter = functions.filter;
 
-exports.testFilterThrows = function(test){
-    test.throws = function(){
+exports.testFilterThrows = function(test) {
+    test.throws = function() {
         filter(even, "dfgfdfg");
     };
     test.done();
 };
 
-exports.testFilter = function(test){
+exports.testFilter = function(test) {
     test.deepEqual([2,4], filter(even, arr));
     test.done();
 };
 
-exports.testFilter1 = function(test){
+exports.testFilter1 = function(test) {
     test.deepEqual([], filter(even, []));
     test.done();
 };
 
-exports.testFilter2 = function(test){
+exports.testFilter2 = function(test) {
     test.deepEqual([], filter(even, [1,3,5]));
     test.done();
 };
 
-exports.testFilter1 = function(test){
+exports.testFilter1 = function(test) {
     test.deepEqual([2,4,6], filter(even, [2,4,6]));
     test.done();
 };
 var reduce = functions.reduce;
 
-exports.testReduceThrows = function(test){
-    test.throws = function(){
+exports.testReduceThrows = function(test) {
+    test.throws = function() {
         reduce(add, "dfgfdfg");
     };
     test.done();
 };
-exports.testReduceThrows1 = function(test){
-    test.throws = function(){
+exports.testReduceThrows1 = function(test) {
+    test.throws = function() {
         reduce(add, []);
     };
     test.done();
 };
 
-exports.testReduce = function(test){
+exports.testReduce = function(test) {
     test.equal(10, reduce(add, arr));
     test.done();
 };
 
 var any = functions.any;
 
-exports.testAnyThrows = function(test){
-    test.throws = function(){
+exports.testAnyThrows = function(test) {
+    test.throws = function() {
         any(even, "dfgfdfg");
     };
     test.done();
 };
 
-exports.testAny = function(test){
+exports.testAny = function(test) {
     test.equal(true, any(even, arr));
     test.done();
 };
 
-exports.testAny1 = function(test){
+exports.testAny1 = function(test) {
     test.equal(false, any(even, [1,5,7]));
     test.done();
 };
 
 var all = functions.all;
 
-exports.testAllThrows = function(test){
-    test.throws = function(){
+exports.testAllThrows = function(test) {
+    test.throws = function() {
         all(even, "dfgfdfg");
     };
     test.done();
 };
 
-exports.testAll = function(test){
+exports.testAll = function(test) {
     test.equal(false, all(even, arr));
     test.done();
 };
 
-exports.testAll1 = function(test){
+exports.testAll1 = function(test) {
     test.equal(true, all(even, [2,4,6]));
     test.done();
 };
--- week0/problems/solutions/higherOrder.js
+++ week0/problems/solutions/higherOrder.js
@@ -1,55 +1,59 @@
-// map, filter, reduce, any, all
-var map = function(f, arr){
-    if(!Array.isArray(arr)){
+ /*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+ 
+var map = function(f, arr) {
+    if(!Array.isArray(arr)) {
         throw new TypeError("The second argument should be array!");
     }
     var result = [];
-    arr.forEach (function( a,i){
+    arr.forEach (function( a,i) {
         result.push(f(a, i));
     });
     return result;
 };
 
-var filter = function(pred,arr){
-    if(!Array.isArray(arr)){
+var filter = function(pred,arr) {
+    if(!Array.isArray(arr)) {
         throw new TypeError("The second argument should be array!");
     }
 
     var result = [];
-    arr.forEach(function(a){
-        if(pred(a)){
+    arr.forEach(function(a) {
+        if(pred(a)) {
             result.push(a);
         }
     });
     return result;
 };
 
-var reduce =  function(f,arr, first){
+var reduce = function(f,arr, first) {
 
-    if(!Array.isArray(arr) || arr.length < 1){
+    if(!Array.isArray(arr) || arr.length < 1) {
         throw new TypeError("The second argument should be not empty array!");
     }
-    if(typeof first != 'undefined'){
+    if(typeof first != 'undefined') {
         accumulate = first;
     }
     else {
         var accumulate = arr.shift();
     }
-    arr.forEach(function(a){
+    arr.forEach(function(a) {
         accumulate = f(accumulate, a);
     });
 
     return accumulate;
 };
 
-var any  = function(f, arr){
-    if(!Array.isArray(arr)){
+var any  = function(f, arr) {
+    if(!Array.isArray(arr)) {
         throw new TypeError("The second argument should be array!");
     }
     var i = 0;
     var n = arr.length;
-    for(i; i< n; i++){
-        if(f(arr[i])){
+    for(i; i< n; i++) {
+        if(f(arr[i])) {
             return true;
         }
     }
@@ -57,14 +61,14 @@ var any  = function(f, arr){
     return false;
 };
 
-var all  = function(f, arr){
-    if(!Array.isArray(arr)){
+var all  = function(f, arr) {
+    if(!Array.isArray(arr)) {
         throw new TypeError("The second argument should be string!");
     }
     var i = 0;
     var n = arr.length;
-    for(i; i< n; i++){
-        if(!f(arr[i])){
+    for(i; i< n; i++) {
+        if(!f(arr[i])) {
             return false;
         }
     }
