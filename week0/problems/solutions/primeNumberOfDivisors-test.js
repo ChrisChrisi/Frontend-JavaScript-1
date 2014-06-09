bcbef709a6108c4ff370e2206da78de2727e3b1b
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 15:55:08 +0300

    edit

--- week0/problems/solutions/primeNumberOfDivisors-test.js
+++ week0/problems/solutions/primeNumberOfDivisors-test.js
@@ -1,34 +1,39 @@
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
 "use strict";
 
 var primeNumberOfDivisors = require("./primeNumberOfDivisors").primeNumberOfDivisors;
 
-exports.testPrimeNumberOfDivisorThrow = function(test){
-    test.throws = function(){
+exports.testPrimeNumberOfDivisorThrow = function (test) {
+    test.throws = function () {
         primeNumberOfDivisors("fhdsdg");
     };
     test.done();
 };
 
-exports.testPrimeNumberOfDivisorThrow1 = function(test){
-    test.throws = function(){
+exports.testPrimeNumberOfDivisorThrow1 = function (test) {
+    test.throws = function () {
         primeNumberOfDivisors(-5);
     };
     test.done();
 };
 
-exports.testPrimeNumberOfDivisorThrow2 = function(test){
-    test.throws = function(){
+exports.testPrimeNumberOfDivisorThrow2 = function (test) {
+    test.throws = function () {
         primeNumberOfDivisors(5.3);
     };
     test.done();
 };
 
-exports.testPrimeNumberOfDivisorTrue = function(test){
-    test.equal(true,primeNumberOfDivisors(7));
+exports.testPrimeNumberOfDivisorTrue = function (test) {
+    test.equal(true, primeNumberOfDivisors(7));
     test.done();
 };
 
-exports.testPrimeNumberOfDivisorFalse = function(test){
-    test.equal(false,primeNumberOfDivisors(8));
+exports.testPrimeNumberOfDivisorFalse = function (test) {
+    test.equal(false, primeNumberOfDivisors(8));
     test.done();
 };
