35afd838075dec3b385dad738c4a210fe3ec7b2d
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 13:16:45 +0300

    foreach solution and test

--- week0/problems/solutions/forEach.js
+++ week0/problems/solutions/forEach.js
@@ -1,1 +1,27 @@
 // solution here
+"use strict";
+
+
+
+var forEach = function(f,arr){
+
+    if(!Array.isArray(arr) || arr.length < 1){
+        throw new TypeError('Invalid parameter');
+    }
+
+    var i = 0;
+
+    var n = arr.length;
+
+    for(i; i< n; i+=1){
+
+      f(arr[i], i, arr);
+
+    }
+
+};
+var addOne = function(a){return a + 1;};
+var arr = [1,2,3];
+console.log(forEach(addOne, arr));
+
+exports.forEach = forEach;
