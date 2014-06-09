401194c0a82029a57efcc5db4cd0d7c0e5d9578d
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 17:38:34 +0300

    error

--- week0/problems/solutions/vowelsInAString.js
+++ week0/problems/solutions/vowelsInAString.js
@@ -3,24 +3,6 @@
  * GitHub - https://github.com/ChrisChrisi
  */
 
-var vowelsInAString = function (str) {
-    if (typeof str !== "string") {
-        throw new TypeError("The input should be string");
-    }
-
-    var result = 0,
-        allVowels = "aeiouy";
-
-    str.toLowerCase()
-        .split("")
-        .forEach(function (a) {
-            if (allVowels.indexOf(a) >= 0) {
-                result += 1;
-            }
-        });
-    return result;
-};
-
 exports.vowelsInAString = vowelsInAString;var vowelsInAString = function (str) {
     if (typeof str !== "string") {
         throw new TypeError("The input should be string");
