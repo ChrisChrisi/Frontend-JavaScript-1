f7e3d91c1822952319901f6dc139ffaa0fd660e6
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 17:13:47 +0300

    better fibo

--- week0/problems/solutions/fib.js
+++ week0/problems/solutions/fib.js
@@ -1,1 +1,21 @@
-// solution here
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
+var fib = function (n) {
+    if (typeof n !== "number" || n % 1 !== 0 || n < 1) {
+        throw new TypeError("The input should be positive integer.")
+    }
+    var cur = 1,
+        next = 1,
+        count = 2;
+    while (count < n) {
+        next = cur + next;
+        cur = next - cur;
+        count += 1;
+    }
+    return next;
+};
+
+exports.fib = fib;
