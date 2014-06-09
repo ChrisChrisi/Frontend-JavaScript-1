b86e9019d3fc4842b6bfa5c6ca188587b9b46de1
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 15:08:00 +0300

    Remove erorrs.

--- week0/problems/solutions/listToNumber-test.js
+++ week0/problems/solutions/listToNumber-test.js
@@ -1,35 +1,43 @@
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
 "use strict";
 
 var listToNumber = require("./listToNumber").listToNumber;
 
-exports.testListToNumberThrow = function(test){
-    test.throws = function(){
+exports.testListToNumberThrow = function (test) {
+    test.throws = function () {
         listToNumber(123);
     };
     test.done();
 };
 
-exports.testListToNumberThrow1 = function(test){
-    test.throws = function(){
+exports.testListToNumberThrow0 = function (test) {
+    test.throws = function () {
+        listToNumber([]);
+    };
+    test.done();
+};
+
+exports.testListToNumberThrow1 = function (test) {
+    test.throws = function () {
         listToNumber("sdgffg");
     };
     test.done();
 };
 
-exports.testListToNumberThrow2 = function(test){
-    test.throws = function(){
-        listToNumber([2,"sdf",3,8]);
+exports.testListToNumberThrow2 = function (test) {
+    test.throws = function () {
+        listToNumber([2, "sdf", 3, 8]);
     };
     test.done();
 };
 
-exports.testListToNumber1 = function(test){
-    test.equal(123,listToNumber([1,2,3]));
+exports.testListToNumber1 = function (test) {
+    test.equal(123, listToNumber([1, 2, 3]));
     test.done();
 };
 
-exports.testListToNumber2 = function(test){
-    test.equal(0,listToNumber([]));
-    test.done();
-};
 
--- week0/problems/solutions/listToNumber.js
+++ week0/problems/solutions/listToNumber.js
@@ -1,22 +1,26 @@
-// solution here
-var listToNumber = function(digits){
-    if(!Array.isArray(digits)){
-       throw new TypeError("the argument should be array");
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
+var listToNumber = function (digits) {
+    if (!Array.isArray(digits)) {
+        throw new TypeError("The argument should be array.");
     }
 
-    if(!digits.every(function(a){
-        return (typeof a === "number"  && a%1 === 0)
-    })){
-        throw new TypeError("the array's elements should be integer number");
+    if (!digits.every(function (a) {
+            return (typeof a === "number" && a % 1 === 0)
+        })) {
+        throw new TypeError("Elements of array should be integers.");
     }
-  if(digits.length < 1){
-      return 0;
-  }
-  var result = digits.reduce(function(a, b){
+    if (digits.length < 1) {
+        throw new TypeError("The array should have at least one element.");
+    }
+    var result = digits.reduce(function (a, b) {
         return a.toString() + b.toString();
     });
-    result = parseInt(result);
-    return result;
+
+    return parseInt(result);
 };
 
 exports.listToNumber = listToNumber;
