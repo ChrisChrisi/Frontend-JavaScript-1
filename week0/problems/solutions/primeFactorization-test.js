945c0e60c0be61a54b1e1c3face5a63be4fca457
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 18:01:23 +0300

    edit

--- week0/problems/solutions/primeFactorization-test.js
+++ week0/problems/solutions/primeFactorization-test.js
@@ -2,38 +2,46 @@
 
 var primeFactorization = require("./primeFactorization").primeFactorization;
 
-exports.testPrimeFactorizationThrow = function(test){
-  test.throws = function(){
-      primeFactorization("adf");
-  };
-  test.done();
-};
-
-exports.testPrimeFactorizationThrow1 = function(test){
-  test.throws = function(){
-      primeFactorization(-5.3);
-  };
-  test.done();
-};
-
-exports.testPrimeFactorizationThrow2 = function(test){
-  test.throws = function(){
-      primeFactorization(-5);
-  };
+exports.testPrimeFactorizationThrow = function (test) {
+    test.throws = function () {
+        primeFactorization("adf");
+    };
     test.done();
 };
 
-exports.testPrimeFactorization1 = function(test){
-    test.deepEqual( [ [2, 1], [5, 1] ], primeFactorization(10));
+exports.testPrimeFactorizationThrow1 = function (test) {
+    test.throws = function () {
+        primeFactorization(-5.3);
+    };
     test.done();
 };
 
-exports.testPrimeFactorization2 = function(test){
-    test.deepEqual( [[2, 2], [89, 1] ], primeFactorization(356));
+exports.testPrimeFactorizationThrow2 = function (test) {
+    test.throws = function () {
+        primeFactorization(-5);
+    };
     test.done();
 };
 
-exports.testPrimeFactorization2 = function(test){
-    test.deepEqual( [[89, 1] ], primeFactorization(89));
+exports.testPrimeFactorization1 = function (test) {
+    test.deepEqual([
+        [2, 1],
+        [5, 1]
+    ], primeFactorization(10));
+    test.done();
+};
+
+exports.testPrimeFactorization2 = function (test) {
+    test.deepEqual([
+        [2, 2],
+        [89, 1]
+    ], primeFactorization(356));
+    test.done();
+};
+
+exports.testPrimeFactorization2 = function (test) {
+    test.deepEqual([
+        [89, 1]
+    ], primeFactorization(89));
     test.done();
 };
--- week0/problems/solutions/primeFactorization.js
+++ week0/problems/solutions/primeFactorization.js
@@ -1,57 +1,83 @@
 // solution here
+
+var throwPositiveIntegerError = function (n) {
+    if (typeof n !== "number" || n % 1 !== 0 || n < 0) {
+        throw new TypeError("The input should be positive integer.");
+    }
+};
+
+var throwIntegerError = function (n) {
+    if (typeof n !== "number" || n % 1 !== 0) {
+        throw new TypeError("The input should be integer.");
+    }
+};
+
+// i didn't find the function range, only _.range
+var range = function (n) {
+    throwPositiveIntegerError(n);
+    return Array
+        .apply(null,
+        {
+            length: (n)
+        })
+        .map(Number.call, Number);
+};
+
 var isPrime = function (n) {
-    if(typeof n !== "number" || n%1 !== 0 || n<0){
-        throw new TypeError("The argument should be positive integer number");
+    throwIntegerError(n);
+    n = Math.abs(n);
+    if (n === 2) {
+        return true;
     }
     if (n === 1) {
         return false;
     }
-    if (n === 2) {
+    var arr = range(n + 1);
+    arr = arr.slice(2);
+
+    var sumOfDivisors = arr
+        .filter(function (a) {
+            return (n % a === 0);
+        }).
+        reduce(function (a, b) {
+            return a + b;
+        });
+    if (sumOfDivisors === n) {
         return true;
     }
-    if (n % 2 === 0) {
-        return false;
-    }
-    var allNums = Array.apply(null, {length: (n)}).map(Number.call, Number).filter(function (a) {
-        return (a % 2 != 0)
-    });
-    allNums = allNums.slice(1);
 
-    return !(allNums.some(function (a) {
-        return (n % a) === 0;
-    }));
+    return false;
 };
 
 var primeFactorization = function (num) {
-    if(typeof num !== "number" || num%1 !== 0 || num<0){
-        throw new TypeError("The argument should be positive integer number");
-    }
+    throwIntegerError(num);
     if (isPrime(num)) {
         return [
             [num, 1]
         ]
     }
-    else {
-        var allNums = Array.apply(null, {length: (num)}).map(Number.call, Number).filter(function (a) {
+    num = Math.abs(num);
+    var allNums = range(num)
+        .filter(function (a) {
             return (a % 2 != 0)
         });
-        allNums = allNums.slice(1);
-        allNums.unshift(2);
-        allNums = allNums.filter(function (a) {
-            return (isPrime(a) && (num % a === 0));
-        });
+    allNums = allNums.slice(1);
+    allNums.unshift(2);
+    allNums = allNums.filter(function (a) {
+        return (isPrime(a) && (num % a === 0));
+    });
 
-        var result =[];
-        allNums.forEach(function(a){
-            var count = 0;
-            while(num % a === 0){
-                count += 1;
-                num = parseInt(num / a);
-            }
-            result.push([a, count]);
-        });
-        return result;
-    }
+    var result = [];
+    allNums.forEach(function (a) {
+        var count = 0;
+        while (num % a === 0) {
+            count += 1;
+            num = parseInt(num / a);
+        }
+        result.push([a, count]);
+    });
+    return result;
+
 };
 
 exports.primeFactorization = primeFactorization;
