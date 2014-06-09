cde7b8677a0f3aeb7166a4d45f0d2e1324610939
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 16:09:06 +0300

    edit

--- week0/problems/solutions/sumOfMinAndMax-test.js
+++ week0/problems/solutions/sumOfMinAndMax-test.js
@@ -1,27 +1,32 @@
-"use strict";
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
 
 var sumOfMinAndMax = require("./sumOfMinAndMax").sumOfMinAndMax;
 
-exports.testSumOfMinAndMaxThrow = function(test){
-    test.throws = function(){
+exports.testSumOfMinAndMaxThrow = function (test) {
+    test.throws = function () {
         sumOfMinAndMax("sdfsd");
     };
     test.done();
 };
 
-exports.testSumOfMinAndMaxThrow1 = function(test){
-    test.throws = function(){
+exports.testSumOfMinAndMaxThrow0 = function (test) {
+    test.throws = function () {
+        sumOfMinAndMax([]);
+    };
+    test.done();
+};
+
+exports.testSumOfMinAndMaxThrow1 = function (test) {
+    test.throws = function () {
         sumOfMinAndMax([1.5, "fdgfd", 8, 4]);
     };
     test.done();
 };
 
-exports.testSumOfMinAndMax1 = function(test){
-    test.equal(0,sumOfMinAndMax([]));
-    test.done();
-};
-
-exports.testSumOfMinAndMax1 = function(test){
-    test.equal(2,sumOfMinAndMax([3.5, 11, -9, 5]));
+exports.testSumOfMinAndMax1 = function (test) {
+    test.equal(2, sumOfMinAndMax([3.5, 11, -9, 5]));
     test.done();
 };
--- week0/problems/solutions/sumOfMinAndMax.js
+++ week0/problems/solutions/sumOfMinAndMax.js
@@ -1,18 +1,28 @@
-// solution here
-var sumOfMinAndMax = function(arr) {
-    if(!Array.isArray(arr)){
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
+var sumOfMinAndMax = function (arr) {
+    if (!Array.isArray(arr)) {
         throw new TypeError("The argument should be string");
     }
 
-    if(!arr.every(function(a){return (typeof a === "number");})){
+    if (!arr.every(function (a) {
+            return (typeof a === "number");
+        })) {
         throw new TypeError("The array elements should be numbers!");
     }
 
-    if(arr.length < 1){
-        return 0;
+    if (arr.length < 1) {
+        throw new TypeError("The array should have at least one element!");
     }
-    var sortedArr = arr.sort(function(a,b){return (a - b);});
-    return (sortedArr[0] +  sortedArr[sortedArr.length - 1] );
+    var sortedArr = arr
+        .sort(function (a, b) {
+            return (a - b)
+                ;
+        });
+    return (sortedArr[0] + sortedArr[sortedArr.length - 1] );
 };
 
 exports.sumOfMinAndMax = sumOfMinAndMax;
