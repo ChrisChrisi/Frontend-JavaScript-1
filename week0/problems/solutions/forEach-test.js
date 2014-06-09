108492409e0e0c83e2dbcfcf110d55ddb5f974c6
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 16:15:24 +0300

    edit spacing

--- week0/problems/solutions/forEach-test.js
+++ week0/problems/solutions/forEach-test.js
@@ -1,21 +1,30 @@
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
 "use strict";
 
 var forEach = require("./forEach").forEach;
 
-var addOne = function(a){return a + 1;};
-var arr = [1,2,3];
+var addOne = function (a) {
+    return a + 1;
+};
+var arr = [1, 2, 3];
 
-exports.testForeachThrow = function(test){
-    test.throws = function(){
-        forEach(addOne,"asdfsd");
+exports.testForeachThrow = function (test) {
+    test.throws = function () {
+        forEach(addOne, "asdfsd");
     };
     test.done();
 };
 
-exports.testForEach = function(test){
+exports.testForEach = function (test) {
     var result = [];
-     forEach(function(a){result.push(a+1);},arr);
-    test.deepEqual([2,3,4],result);
+    forEach(function (a) {
+        result.push(a + 1);
+    }, arr);
+    test.deepEqual([2, 3, 4], result);
     test.done();
 };
 
--- week0/problems/solutions/forEach.js
+++ week0/problems/solutions/forEach.js
@@ -1,27 +1,19 @@
-// solution here
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
 "use strict";
+var forEach = function (f, arr) {
 
-
-
-var forEach = function(f,arr){
-
-    if(!Array.isArray(arr) || arr.length < 1){
+    if (!Array.isArray(arr) || arr.length < 1) {
         throw new TypeError('Invalid parameter');
     }
-
-    var i = 0;
-
-    var n = arr.length;
-
-    for(i; i< n; i+=1){
-
-      f(arr[i], i, arr);
-
+    var i = 0,
+        n = arr.length;
+    for (i; i < n; i += 1) {
+        f(arr[i], i, arr);
     }
-
 };
-var addOne = function(a){return a + 1;};
-var arr = [1,2,3];
-console.log(forEach(addOne, arr));
 
 exports.forEach = forEach;
