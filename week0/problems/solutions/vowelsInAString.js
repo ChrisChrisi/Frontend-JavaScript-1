6ffb8c9a66ec7ac3412a62a7040f2e0478638c22
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 17:37:50 +0300

    error fix

--- week0/problems/solutions/vowelsInAString.js
+++ week0/problems/solutions/vowelsInAString.js
@@ -21,4 +21,22 @@ var vowelsInAString = function (str) {
     return result;
 };
 
+exports.vowelsInAString = vowelsInAString;var vowelsInAString = function (str) {
+    if (typeof str !== "string") {
+        throw new TypeError("The input should be string");
+    }
+
+    var result = 0,
+        allVowels = "aeiouy";
+
+    str.toLowerCase()
+        .split("")
+        .forEach(function (a) {
+            if (allVowels.indexOf(a) >= 0) {
+                result += 1;
+            }
+        });
+    return result;
+};
+
 exports.vowelsInAString = vowelsInAString;
