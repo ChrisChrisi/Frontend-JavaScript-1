24b6576b1cc6ee7c952e71493fa85545c898864a
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 16:12:33 +0300

    align anad edit

--- week0/problems/solutions/vowelsInAString.js
+++ week0/problems/solutions/vowelsInAString.js
@@ -1,19 +1,24 @@
-// solution here
+/*
+ * Solution by Christina Mileva
+ * GitHub - https://github.com/ChrisChrisi
+ */
+
 var allVowels = "aeiouy";
 
-var vowelsInAString= function(str){
-    if(typeof str !== "string"){
+var vowelsInAString = function (str) {
+    if (typeof str !== "string") {
         throw new TypeError("The input should be string");
     }
 
-    var letters = str.toLowerCase().split("");
     var result = 0;
 
-    letters.forEach(function(a){
-        if(allVowels.indexOf(a) >= 0){
-            result += 1;
-        }
-    });
+    str.toLowerCase()
+        .split("")
+        .forEach(function (a) {
+            if (allVowels.indexOf(a) >= 0) {
+                result += 1;
+            }
+        });
     return result;
 };
 
