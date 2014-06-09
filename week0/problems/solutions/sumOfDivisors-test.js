1a4fac4701805c4903218c1d38ef57d7eb1b976d
ChrisChrisi <kristina_milev@abv.bg>
2014-06-09 10:50:32 +0300

    test added

---
+++ week0/problems/solutions/sumOfDivisors-test.js
@@ -0,0 +1,32 @@
+"use strict";
+
+var sumOfDivisors = require("./sumOfDivisors").sumOfDivisors;
+
+exports.testSumOfDivisorsThrow = function(test){
+    test.throws = function(){
+        sumOfDivisors("dsffsa");
+    };
+    test.done();
+};
+
+exports.testSumOfDivisorsThrow1 = function(test){
+    test.throws = function(){
+        sumOfDivisors(5.3);
+    };
+    test.done();
+};
+
+exports.testSumOfDivisors = function(test){
+    test.equal(15,sumOfDivisors(8));
+    test.done();
+};
+
+exports.testSumOfDivisors1 = function(test){
+    test.equal(8,sumOfDivisors(7));
+    test.done();
+};
+
+exports.testSumOfDivisors = function(test){
+    test.equal(1,sumOfDivisors(1));
+    test.done();
+};
--- week0/problems/solutions/sumOfDivisors.js
+++ week0/problems/solutions/sumOfDivisors.js
@@ -1,9 +1,13 @@
 // solution here
-var sum_of_divisors = function(n) {
-
+var sumOfDivisors = function(n) {
+if(typeof n !== "number" || n%1 !== 0){
+    throw new TypeError("The input should be integer!");
+}
 // generate array of numbers from 1 to n
     var arr = Array.apply(null, {length: Math.abs(n+1)}).map(Number.call, Number);
     arr = arr.slice(1);
 
     return arr.filter(function(a){return (n % a === 0); }).reduce(function(a,b){ return a + b;});
 };
+
+exports.sumOfDivisors = sumOfDivisors;
